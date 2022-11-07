784033fdb81d230190a1963de43cc4f96ebaca0c
zaoravi1 <zaorvi@seznam.cz>
2022-11-07 15:16:17 +0100

    cv 8 asi

--- pro2_projekt-main/src/main/java/Main.java
+++ pro2_projekt-main/src/main/java/Main.java
@@ -1,12 +1,32 @@
+import models.Message;
 import models.chatClients.ChatClient;
 import models.chatClients.FileChatClient;
 import models.chatClients.InMemoryChatClient;
 import models.chatClients.fileOperations.ChatFileOperations;
 import models.chatClients.fileOperations.JsonChatFileOperations;
+import models.database.DatabaseOperations;
+import models.database.DbInitializer;
+import models.database.JdbcDatabaseOperations;
 import models.gui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
+        String databaseDriver = "org.apache.derby.jdbc.EmbeddedDriver";
+        String databaseUrl = "jdbc:derby:ChatClientDb_skC";
+
+        DbInitializer dbInitializer = new DbInitializer(databaseDriver, databaseUrl);
+        dbInitializer.init();
+
+        try{
+            DatabaseOperations databaseOperations =
+                    new JdbcDatabaseOperations(databaseDriver, databaseUrl);
+
+            //databaseOperations.addMessage(new Message("Langer", "pokusný text"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //testChat
         ChatFileOperations chatFileOperations = new JsonChatFileOperations();
         ChatClient chatClient = new FileChatClient(chatFileOperations);
         MainFrame frame = new MainFrame(800,600, chatClient);
